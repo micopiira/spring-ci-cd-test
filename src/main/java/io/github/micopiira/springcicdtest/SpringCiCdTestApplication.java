e55975cd9bde4f6d5fd22e1bab4321d480c6d1a6
Mico Piira <mico.piira@disec.fi>
2022-02-06 13:07:04 +0200

    first commit

---
+++ src/main/java/io/github/micopiira/springcicdtest/SpringCiCdTestApplication.java
@@ -0,0 +1,13 @@
+package io.github.micopiira.springcicdtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCiCdTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCiCdTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/micopiira/springcicdtest/SpringCiCdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.micopiira.springcicdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCiCdTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
